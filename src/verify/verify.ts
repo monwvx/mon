ed835db38e7b75604d1cf3b6fc329b44a3ed56b3
monwvx <monwvx@gmail.com>
2020-07-27 15:36:49 +0800

    update verify

--- src/verify/verify.ts
+++ src/verify/verify.ts
@@ -52,8 +52,30 @@ class Verify {
     return false
   }
 
-  // public
+  // 邮箱验证
+  isEmail = (s: string) => {
+    return /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((.[a-zA-Z0-9_-]{2,3}){1,2})$/.test(s)
+  }
 
+  // 手机号码验证
+  isMobile = (s: string) => {
+    return /^1[0-9]{10}$/.test(s)
+  }
+
+  // 电话号码验证
+  isPhone = (s: string) => {
+    return /^([0-9]{3,4}-)?[0-9]{7,8}$/.test(s)
+  }
+
+  // url验证
+  isURL = (s: string) => {
+    return /^http[s]?:\/\/.*/.test(s)
+  }
+
+  //ip验证
+  isIP = (s: string) => {
+    return /^(?:(?:2[0-4][0-9]\.)|(?:25[0-5]\.)|(?:1[0-9][0-9]\.)|(?:[1-9][0-9]\.)|(?:[0-9]\.)){3}(?:(?:2[0-4][0-9])|(?:25[0-5])|(?:1[0-9][0-9])|(?:[1-9][0-9])|(?:[0-9]))$/.test(s)
+  }
 }
 
 let verify = new Verify()
